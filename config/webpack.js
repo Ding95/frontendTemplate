5950e967330a1b51a988a247c5d28e5820e2f8f3
adain <1154554396@qq.com>
2019-05-20 16:29:37 +0800

    f

--- config/webpack.js
+++ config/webpack.js
@@ -1,9 +1,33 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, '../src/js/index.js'),
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../src/views/template.html')
+    })
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, '../dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.less$/,
+        loader: 'style-loader!css-loader!less-loader'
+      }
+    ]
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
-console.log('hello world')
+import '../styles/index.css'
+import '../styles/main.less'
+
+import avatar from '../assets/images/0.jpg'
+
+const component = () => {
+  const element = document.createElement('div')
+  const element2 = document.createElement('a')
+  const img = document.createElement('img')
+  img.src = avatar
+  element2.innerText = '百度一下，你就知道'
+  element2.href = 'https://www.baidu.com'
+  element.innerHTML = 'hello webpack'
+  element.appendChild(element2)
+  element.appendChild(img)
+  element.classList.add('hello')
+
+  return element
+}
+
+document.body.appendChild(component())
