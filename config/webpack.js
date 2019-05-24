5202b452d3e24e2b896b0c84ca2948026fd701db
adain <1154554396@qq.com>
2019-05-24 10:14:50 +0800

    f

--- config/webpack.js
+++ config/webpack.js
@@ -4,6 +4,10 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, '../src/js/index.js'),
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, '../dist')
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
